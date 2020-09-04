c1c890e909a8a7fdd7e3cc259f374938d001cb08
Bensas <juanbensa@gmail.com>
2020-09-04 18:42:01 -0300

    Renamed aptitudeFunction in CharacterClass

--- src/character-classes/character-class.ts
+++ src/character-classes/character-class.ts
@@ -1,19 +1,19 @@
 export abstract class CharacterClass {
-  abstract aptitude: (attack: number, defense: number) => number;
+  abstract aptitudeFunction: (attack: number, defense: number) => number;
 }
 
 export class Warrior extends CharacterClass {
-  aptitude = (attack: number, defense: number) => 0.6 * attack + 0.6 * defense;
+  aptitudeFunction = (attack: number, defense: number) => 0.6 * attack + 0.6 * defense;
 }
 
 export class Archer extends CharacterClass {
-  aptitude = (attack: number, defense: number) => 0.9 * attack + 0.1 * defense;
+  aptitudeFunction = (attack: number, defense: number) => 0.9 * attack + 0.1 * defense;
 }
 
 export class Tank extends CharacterClass {
-  aptitude = (attack: number, defense: number) => 0.3 * attack + 0.8 * defense;
+  aptitudeFunction = (attack: number, defense: number) => 0.3 * attack + 0.8 * defense;
 }
 
 export class Spy extends CharacterClass {
-  aptitude = (attack: number, defense: number) => 0.8 * attack + 0.3 * defense;
+  aptitudeFunction = (attack: number, defense: number) => 0.8 * attack + 0.3 * defense;
 }

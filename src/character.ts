1faa16e41fc5b0f59db5634e83b568871924f7e0
Bensas <juanbensa@gmail.com>
2020-09-05 00:29:02 -0300

    Modified character methods to work with map gene

--- src/character.ts
+++ src/character.ts
@@ -2,59 +2,82 @@ import { CharacterClass } from "./character-classes/character-class";
 import { Item } from "./items/item";
 import { isNumber } from "util";
 
+const HEIGHT = 'HEIGHT';
+const HELMET = 'helmet';
+const WEAPON = 'weapon';
+const BOOTS = 'boots';
+const GLOVES = 'gloves';
+const BREASTPLATE = 'breastplate';
+
 export class Character {
   class: CharacterClass;
-  genes: {
-    height: number,
-    helmet: Item,
-    weapon: Item,
-    boots: Item,
-    gloves: Item,
-    breastplate: Item
-  }
+  genes: Map<string, number | Item>;
 
-  // constructor(charClass?: CharacterClass, charGenes?: )
+  // constructor(charClass?: CharacterClass, charGenes?)
+  constructor() {
+    this.genes = new Map();
+  }
 
   public getAptitude(): number {
     return this.class.aptitudeFunction(this.getAttack(), this.getDefense());
   }
 
   private getStrength(){
-    var suma: number = this.genes.helmet.strength + this.genes.weapon.strength + this.genes.boots.strength + this.genes.gloves.strength + this.genes.breastplate.strength;
+    let suma: number = 0;
+    this.genes.forEach((value, key) => {
+      if (typeof value !== 'number')
+        suma += (<Item>value).strength;
+    });
     var strength: number = 100 * Math.tanh(0.01 * suma);
     return strength;
   }
 
   private getHealth(){
-    var suma: number = this.genes.helmet.health + this.genes.weapon.health + this.genes.boots.health + this.genes.gloves.health + this.genes.breastplate.health;
+    let suma: number = 0;
+    this.genes.forEach((value, key) => {
+      if (typeof value !== 'number')
+        suma += (<Item>value).health;
+    });
     var health: number = 100 * Math.tanh(0.01 * suma);
     return health;
   }
 
   private getAgility(){
-    var suma: number = this.genes.helmet.agility + this.genes.weapon.agility + this.genes.boots.agility + this.genes.gloves.agility + this.genes.breastplate.agility;
+    let suma: number = 0;
+    this.genes.forEach((value, key) => {
+      if (typeof value !== 'number')
+        suma += (<Item>value).agility;
+    });
     var agility: number = Math.tanh(0.01 * suma);
     return agility;
   }
 
   private getExp(){
-    var suma: number = this.genes.helmet.exp + this.genes.weapon.exp + this.genes.boots.exp + this.genes.gloves.exp + this.genes.breastplate.exp;
+    let suma: number = 0;
+    this.genes.forEach((value, key) => {
+      if (typeof value !== 'number')
+        suma += (<Item>value).exp;
+    });
     var strength: number = 0.6 * Math.tanh(0.01 * suma);
     return strength;
   }
 
   private getResistance(){
-    var suma: number = this.genes.helmet.resistance + this.genes.weapon.resistance + this.genes.boots.resistance + this.genes.gloves.resistance + this.genes.breastplate.resistance;
+    let suma: number = 0;
+    this.genes.forEach((value, key) => {
+      if (typeof value !== 'number')
+        suma += (<Item>value).resistance;
+    });
     var strength: number = Math.tanh(0.01 * suma);
     return strength;
   }
 
   private getATM(){
-    return 0.7 - Math.pow(3 * this.genes.height - 5, 4) + Math.pow(3 * this.genes.height - 5, 2) + this.genes.height/4;
+    return 0.7 - Math.pow(3 * <number>this.genes.get(HEIGHT) - 5, 4) + Math.pow(3 * <number>this.genes.get(HEIGHT) - 5, 2) + <number>this.genes.get(HEIGHT)/4;
   }
 
   private getDFM(){
-    return 1.9 + Math.pow(2.5 * this.genes.height - 4.16, 4) - Math.pow(2.5 * this.genes.height - 4.16, 2) - (3 * this.genes.height) / 4;
+    return 1.9 + Math.pow(2.5 * <number>this.genes.get(HEIGHT) - 4.16, 4) - Math.pow(2.5 * <number>this.genes.get(HEIGHT) - 4.16, 2) - (3 * <number>this.genes.get(HEIGHT)) / 4;
   }
 
   getAttack(){

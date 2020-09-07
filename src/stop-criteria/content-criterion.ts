e80e713dbe0637eaee83e442be19e24420ba89e5
Yatex <foliver@gmail.com>
2020-09-07 16:41:44 -0300

    saque console.log

--- src/stop-criteria/content-criterion.ts
+++ src/stop-criteria/content-criterion.ts
@@ -5,7 +5,6 @@ export function contentCriterion(geneticEngine: GeneticEngine): boolean {
     if( tam < geneticEngine.config.stopValue)
         return false;
     let plagioFitnessControl: number[] = geneticEngine.metrics.historicalMaxFitness.slice(tam - geneticEngine.config.stopValue);
-    console.log('el largo es: ' + geneticEngine.metrics.historicalMaxFitness);
     for(let i = 1; i < geneticEngine.config.stopValue; i++){
         if(plagioFitnessControl[0] !== plagioFitnessControl[i]){
             return false;
